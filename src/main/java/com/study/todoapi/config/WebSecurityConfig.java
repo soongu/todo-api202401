b72623c7da1e1b47fe2e3c2fec2b4dc3fcfbecdf
soongu <naong526@naver.com>
2024-01-24 11:30:02 +0900

    #10. 토큰 위조 검사 및 클레임 파싱

--- src/main/java/com/study/todoapi/config/WebSecurityConfig.java
+++ src/main/java/com/study/todoapi/config/WebSecurityConfig.java
@@ -1,8 +1,10 @@
 package com.study.todoapi.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -23,7 +25,18 @@ public class WebSecurityConfig {
                 .cors()
                 .and()
                 .csrf().disable()
-                .httpBasic().disable();
+                .httpBasic().disable()
+                // 세션 인증은 더이상 사용하지 않겠다
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                // 어떤 요청에서는 인증을하고 어떤 요청에서는 인증을 안할 건지 설정
+                .authorizeRequests() // 어떤 요청에서 인증을 할 거냐??
+                    .antMatchers("/", "/api/auth/**").permitAll() // 이 요청은 인증을 안해도 됨!
+                    //.antMatchers(HttpMethod.POST, "/api/todos").permitAll()
+                    //.antMatchers("/**").hasRole("ADMIN")
+                .anyRequest().authenticated() // 나머지 요청은 모두 인증(로그인)받고 해!!
+        ;
 
         return http.build();
     }
